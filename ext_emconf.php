e2cb4b1f583016dcedc0ce7b80763a8171b8e2bf
Mike Street <mike@liquidlight.co.uk>
2021-11-05 09:42:07 +0000

    Chore: Add details to composer and ex_empconf
    Also make placeholder upcoming & changelog files

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
 	'title' => 'Call to Actions',
 	'description' => 'Shared Call to Action repository and plugin, to allow display of your CTAs',
-	'category' => 'fe',
+	'category' => 'plugin',
 	'author' => 'Mike Street',
 	'author_company' => 'Liquid Light Ltd',
 	'author_email' => 'mike@liquidlight.co.uk',
